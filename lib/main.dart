9a3603a419f8c05b895e6d026bf0dc856cafe200
MathematicalFunctionANUBHAV <anubhavvermadelhi@gmail.com>
2024-01-01 18:02:30 +0530

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:nova/firebase_options.dart';
 import 'package:nova/views/login_view.dart';
 import 'package:nova/views/register_view.dart';
 import 'package:nova/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 // Import the Firebase Core package
 
 void main() {
@@ -12,11 +13,12 @@ void main() {
   runApp(MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 22, 210, 110)),
-        useMaterial3: true,
-        primarySwatch: Colors.indigo,
-      ),
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.lightGreen,
+          ),
+          scaffoldBackgroundColor: const Color(0xFFFFF8E1),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Color.fromARGB(255, 255, 125, 125))),
       home: const HomePage(),
       routes: {
         '/login/': (context) => const LoginView(),
@@ -39,25 +41,88 @@ class HomePage extends StatelessWidget {
               final user = FirebaseAuth.instance.currentUser;
               if (user != null) {
                 if (user.emailVerified) {
-                  print('Email is verified');
+                  return const NotesView();
                 } else {
                   return const VerifyEmailView();
                 }
               } else {
                 return const LoginView();
               }
-              // if (user?.emailVerified ?? false) {
-              //   return const Text(
-              //       "You're already Verified, chill out mate");
-              // } else {
-              //   print(user);
-              //   return const VerifyEmailView();
-              // }
-              return const Text("Done");
+            // if (user?.emailVerified ?? false) {
+            //   return const Text(
+            //       "You're already Verified, chill out mate");
+            // } else {
+            //   print(user);
+            //   return const VerifyEmailView();
+            // }
 
             default:
               return const Text("Loading...");
           }
         });
   }
+}
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Main UI'),
+          actions: [
+            PopupMenuButton<MenuAction>(onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogoutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login/', (route) => false);
+                  }
+                  devtools.log(shouldLogout.toString());
+                  break;
+                // TODO: Handle this case.
+              }
+            }, itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                    value: MenuAction.logout, child: Text("Log Out"))
+              ];
+            })
+          ],
+        ),
+        body: const Text("shut up idiot?"));
+  }
+}
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+          title: const Text('Sign Out'),
+          content: const Text('Are you sure you want to sign out?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Log out'))
+          ]);
+    },
+  ).then((value) => value ?? false);
 }
